fd12b8c4fb065692f35d8d5c534b2e59b0da4255
Mancaster06 <rajabuomary061@gmail.com>
2024-03-07 02:12:50 -0800

    initial commit

---
+++ main.dart
@@ -0,0 +1,23 @@
+void main() {
+  //int to store numbers
+  int age = 25;
+  print("I am $age years old");
+
+  double height = 5.9;
+  //double to  store decimal numbers
+  print("I am $height tall");
+
+  String name = "Rajabu";
+  // string to store text
+  print("My name is $name");
+
+  List<String> family = ["Saidi", "Omary", "Rahma"];
+  //list to store ordered collection of items
+  print("These are my family members: $family");
+
+  Map<String, int> grades = {"Math": 90, "Physics": 92, "Chemistry": 67};
+  //map to store key value pairs
+  print("My grades are : $grades");
+  //accessing elements from data structure
+  print("Our firstborn is $family[1]");
+}
